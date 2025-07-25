fac82ed433c33573df74065c40e91d4e32816ca2
osamh09 <osamaForEdu@gmail.com>
2025-07-25 10:40:18 +0300

    An AI Translator App

---
+++ Translation App/index.js
@@ -0,0 +1,125 @@
+
+let choosenLanguage
+
+document.querySelectorAll('input[type=radio]').forEach(
+    radioBtn => radioBtn.addEventListener(
+    'click', function(e){
+        choosenLanguage = e.target.value
+    }
+))
+
+document.getElementById("translate-btn").addEventListener('click', 
+    function(){
+        const inputText = document.getElementById("user-input").value
+        document.getElementById("user-input").value = ''
+        if(choosenLanguage && inputText)
+            translateTo(choosenLanguage, inputText)
+        else{
+                const warning = document.createElement('h3')
+                warning.classList.add("warning")
+                warning.id = "warning"
+                warning.innerHTML =`Something went wrong<br/> make sure you entered a text` 
+                document.querySelector("main").appendChild(warning)
+                setTimeout( function(){
+                    document.getElementById("warning").remove()
+                },2500)
+            }              
+    }
+)
+
+
+
+async function translateTo(lang, inputText){
+    
+    const messages = [
+        {
+            role:"system", 
+            content:`You are a linguist with knowledge of every language in existence. 
+            You can flawlessly translate any sentence into ${lang}, providing clear, easy-to-understand,
+            and logically accurate meanings — optionally offering definitions for rare or complex word`
+        },
+        {
+            role:"user",
+            content: `${inputText}`
+        }]
+        
+        try{
+            const url = "https://translation-ai-app.osamaforedu.workers.dev"
+            const res = await fetch(url, 
+                {
+                    method: "POST", 
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(messages)
+                }
+                
+            )
+            if(!res.ok)
+                throw new Error(`Worker Error: ${data.error}`)
+
+            const data = await res.json() 
+            console.log(data)
+            renderOutputtml(data.content);
+
+
+        } catch(e){
+            document.querySelector('main').innerHTML = `${e}`
+        }
+
+        
+    
+
+}
+
+function renderOutputtml(translatedText){
+    const responseHtml = 
+    `
+        <p class="text-field">${translatedText}</p>
+        <button id="start-over">Start Over</button>
+    `
+    document.getElementById('translated-text').innerHTML = responseHtml
+    
+document.getElementById("start-over").addEventListener('click', function()
+    {
+        document.getElementById('translated-text').innerHTML = `
+         <fieldset class="languages-options">
+                    <legend class="select-language">Select language 👇</legend>
+                        <div>
+                            <input 
+                            type="radio"
+                            class="option"
+                            id="arabic"
+                            value="arabic"
+                            name="lang" />
+                            
+                            <label for="arabic">Arabic <img src="./assets/tn_palestine-flag.gif" class="language-flag"></label>
+                        </div>
+                    
+                        <div>
+                            <input type="radio" 
+                            class="option" 
+                            id="japanese" 
+                            value="japanese" 
+                            name="lang" />
+                            
+                            <label for="japanese">Japanese <img src="./assets/jpn-flag.png" class="language-flag"></label>
+                        
+                        </div>
+                        
+                        <div>
+
+                            <input type="radio" 
+                            class="option" 
+                            id="spanish" 
+                            value="spanish" 
+                            name="lang"/>
+                            
+                            <label for="spanish">Spanish <img src="./assets/sp-flag.png" class="language-flag"></label>
+                        </div>
+                </fieldset>
+                <button id="translate-btn">Translate</button>
+        `
+    })
+
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI  from "openai";
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type'
+};
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
+		}
+		if(request.method !== 'POST'){
+			return new Response(JSON.stringify({error: `${request.method} Methode not allowed`}),
+		{
+			status: 405, 
+			headers: corsHeaders
+		})
+		}
+
+		const openai = new OpenAI({
+			apiKey: env.OPENAI_API_KEY, 
+			baseURL: "https://gateway.ai.cloudflare.com/v1/aec8d2f0bb85a6bc96f15f2c0b835767/translate_ai_app/openai"
+	
+		})
+
+
+		
+		try{
+			const messages = await request.json()
+
+			const response = await openai.chat.completions.create({
+				model: "gpt-3.5-turbo",
+				messages,
+				temperature: 1.1, 
+				presence_penalty: 0,
+				frequency_penalty: 0
+				
+			});
+			return new Response(JSON.stringify(response.choices[0].message), { headers: corsHeaders });
+		} catch(e){
+		
+			return new Response(JSON.stringify({error: e.message}), {status: 500,  headers: corsHeaders });
+			
+		}
 	},
 };
